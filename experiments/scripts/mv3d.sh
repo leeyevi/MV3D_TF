8c317e8d4793e4ca4684f722b9a9cc3cae17badc
yuan gao <yuan.gao@uisee.com>
2017-05-08 15:17:56 +0800

    add script for mv3d
    usage: ./experiments/scripts/mv3d gpu 0 network.ckpt.meta kitti_train/kitti_test

--- experiments/scripts/mv3d.sh
+++ experiments/scripts/mv3d.sh
@@ -14,7 +14,7 @@ export PYTHONUNBUFFERED="True"
 
 DEV=$1
 DEV_ID=$2
-NET=$3
+WEIGHTS=$3
 DATASET=$4
 
 array=( $@ )
@@ -24,25 +24,26 @@ EXTRA_ARGS_SLUG=${EXTRA_ARGS// /_}
 
 
 
-LOG="experiments/logs/mv3d_end2end_${NET}_${EXTRA_ARGS_SLUG}.txt.`date +'%Y-%m-%d_%H-%M-%S'`"
+LOG="experiments/logs/mv3d_end2end_.txt.`date +'%Y-%m-%d_%H-%M-%S'`"
 exec &> >(tee -a "$LOG")
 echo Logging output to "$LOG"
 
-time python ./tools/train_net.py --device ${DEV} --device_id ${DEV_ID} \
-  --weights data/pretrain_model/VGG_imagenet.npy \
-  --imdb 'kitti_train' \
-  --iters 10000 \
-  --cfg experiments/cfgs/faster_rcnn_end2end.yml \
-  --network MV3D_train \
-  ${EXTRA_ARGS}
+# time python ./tools/train_net.py --device ${DEV} --device_id ${DEV_ID} \
+  # --weights ${WEIGHTS}\
+  # --imdb ${DATASET} \
+  # --iters 10001\
+  # --cfg experiments/cfgs/faster_rcnn_end2end.yml \
+  # --network MV3D_train \
+  # ${EXTRA_ARGS}
 
+  # --weights data/pretrain_model/vgg_imagenet_sampled.npy \
 set +x
 NET_FINAL=`grep -B 1 "done solving" ${LOG} | grep "Wrote snapshot" | awk '{print $4}'`
 set -x
 
-# time python ./tools/test_net.py --device ${DEV} --device_id ${DEV_ID} \
-  # --weights /home/radmin/code/Faster-RCNN_TF/output/faster_rcnn_end2end/train/VGGnet_fast_rcnn_iter_14000.ckpt.meta \
-  # --imdb kitti_test \
-  # --cfg experiments/cfgs/faster_rcnn_end2end.yml \
-  # --network MV3D_test \
-  # ${EXTRA_ARGS}
+time python ./tools/test_net.py --device ${DEV} --device_id ${DEV_ID} \
+  --weights ${WEIGHTS} \
+  --imdb ${DATASET}\
+  --cfg experiments/cfgs/faster_rcnn_end2end.yml \
+  --network MV3D_test \
+  ${EXTRA_ARGS}
